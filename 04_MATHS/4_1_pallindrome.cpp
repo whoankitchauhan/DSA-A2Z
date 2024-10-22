a4881be059a4c9ab9d6bc30d3853ec7dd62f304d
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-10-22 23:41:51 +0530

    checking Pallindrome With only Reverse half of the number.

---
+++ 04_MATHS/4_1_pallindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int n)
+{
+    // Negative numbers are not palindromes
+    if (n < 0)
+        return false;
+
+    int reversedHalf = 0;
+
+    // Reverse half of the number
+    while (n > reversedHalf)
+    {
+        int digit = n % 10;
+        reversedHalf = reversedHalf * 10 + digit;
+        n = n / 10;
+    }
+
+    // Check if the original first half is equal to the reversed second half
+    return (n == reversedHalf || n == reversedHalf / 10);
+}
+
+int main()
+{
+    cout << "Enter the Number: ";
+    int num;
+    cin >> num;
+
+    if (isPalindrome(num))
+    {
+        cout << "The number " << num << " is a Palindrome." << endl;
+    }
+    else
+    {
+        cout << "The number " << num << " is not a Palindrome." << endl;
+    }
+
+    return 0;
+}
