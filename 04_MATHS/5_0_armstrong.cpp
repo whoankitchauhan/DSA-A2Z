fba522c2d3072a52d346a52da7fb06868b7edb8d
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-10-23 22:19:30 +0530

    Armstrong Brute Solution

---
+++ 04_MATHS/5_0_armstrong.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath> // For pow() function
+using namespace std;
+
+// Function to check if a number is an Armstrong number
+bool isArmstrong(int n)
+{
+    int originalNumber = n;
+    int sum = 0;
+    int numberOfDigits = 0;
+
+    // Find the number of digits in the number
+    int temp = n;
+    while (temp != 0)
+    {
+        temp /= 10;
+        numberOfDigits++;
+    }
+
+    // Calculate the sum of digits raised to the power of numberOfDigits
+    temp = n;
+    while (temp != 0)
+    {
+        int digit = temp % 10;
+        sum += pow(digit, numberOfDigits);
+        temp /= 10;
+    }
+
+    // Check if the sum is equal to the original number
+    return (sum == originalNumber);
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isArmstrong(num))
+    {
+        cout << num << " is an Armstrong number." << endl;
+    }
+    else
+    {
+        cout << num << " is not an Armstrong number." << endl;
+    }
+
+    return 0;
+}
