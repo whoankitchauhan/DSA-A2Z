2dbb7537435e3e23834c8a65b577f7bf649251fb
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-10-25 22:31:40 +0530

    GCD - Calculating from Backward

--- 04_MATHS/8_0_gcd.cpp
+++ 04_MATHS/8_0_gcd.cpp
@@ -28,3 +28,23 @@ int main()
 
     return 0;
 }
+
+/*
+Staring From Back Side - Efficient
+int gcd(int a, int b)
+{
+    int min_num = (a < b) ? a : b; // Find the smaller of the two numbers
+    int result = 1;                // Initialize GCD to 1
+
+    // Check every number from 1 to min_num
+    for (int i = min_num; i >= 1; i--)
+    {
+        if (a % i == 0 && b % i == 0)
+        {               // If i divides both a and b
+            result = i; // Update result to the current divisor
+            break;
+        }
+    }
+    return result; // Return the GCD
+}
+*/
