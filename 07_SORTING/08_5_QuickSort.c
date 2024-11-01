5f2a11323c804b6d6a814daf3f5e4648b8f0b6ad
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-11-01 21:56:52 +0530

    Quick Sort

---
+++ 07_SORTING/08_5_QuickSort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// Partition function for Quick Sort
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+// Function to perform Quick Sort
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, size);
+
+    quickSort(arr, 0, size - 1);
+
+    printf("Sorted array using Quick Sort:\n");
+    printArray(arr, size);
+
+    return 0;
+}
