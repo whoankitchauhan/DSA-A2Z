1df823082d8ddb16f4e6d7575c2a919772f1779d
Ankit Chauhan <138862610+whoankitchauhan@users.noreply.github.com>
2024-12-30 20:25:30 +0530

    integer Hashiing using Map

---
+++ 06_HASHING/3_0_intMap.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    // Input the size of the array
+    int size;
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+
+    int numbers[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> numbers[i];
+    }
+
+    // Map to store the frequency of each number
+    map<int, int> frequencyMap;
+
+    // Populate the map with the frequency of each number
+    for (int i = 0; i < size; i++)
+    {
+        frequencyMap[numbers[i]]++;
+    }
+
+    // Print all key-value pairs in the frequency map
+    cout << "Frequency of each number in the array:" << endl;
+    for (auto it = frequencyMap.begin(); it != frequencyMap.end(); it++)
+    {
+        cout << "Number: " << it->first << ", Count: " << it->second << endl;
+    }
+
+    // Query the frequency of numbers
+    int queryCount;
+    cout << "Enter the number of queries: ";
+    cin >> queryCount;
+
+    while (queryCount--)
+    {
+        int queryNumber;
+        cout << "Enter a number to query its frequency: ";
+        cin >> queryNumber;
+
+        // Retrieve and display the frequency of the queried number
+        cout << "Frequency of " << queryNumber << " in the array: "
+             << frequencyMap[queryNumber] << endl;
+    }
+
+    return 0;
+}
