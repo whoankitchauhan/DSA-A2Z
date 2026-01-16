c1a35ce5f7a24af72fe4a6bb3a4fea5f58087182
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-01-16 19:22:13 +0530

    feat: binary search on max pages using greedy allocation feasibility | TC: O(n log(sum − max)), SC: O(1)

---
+++ 09_BINARY SEARCH/21_1_allocateBooks.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canAllocateBooks(vector<int> &books, int students, int maxPages)
+{
+    int studentsUsed = 1;
+    int pagesAllocated = 0;
+
+    for (int pagesInBook : books)
+    {
+        if (pagesInBook > maxPages)
+            return false;
+
+        if (pagesAllocated + pagesInBook > maxPages)
+        {
+            studentsUsed++;
+            pagesAllocated = pagesInBook;
+
+            if (studentsUsed > students)
+                return false;
+        }
+        else
+        {
+            pagesAllocated += pagesInBook;
+        }
+    }
+    return true;
+}
+
+int allocateBooks(vector<int> &books, int students)
+{
+    if (students > books.size())
+        return -1;
+
+    int low = *max_element(books.begin(), books.end());
+    int high = accumulate(books.begin(), books.end(), 0);
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (canAllocateBooks(books, students, mid))
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+void printVector(const vector<int> &v)
+{
+    cout << "[";
+    for (int i = 0; i < v.size(); ++i)
+    {
+        cout << v[i];
+        if (i + 1 < v.size())
+            cout << ", ";
+    }
+    cout << "]";
+}
+
+int main()
+{
+    vector<vector<int>> tests = {
+        {12, 34, 67, 90}, // classic
+        {10, 20, 30, 40}, // evenly increasing
+        {5, 5, 5, 5},     // uniform
+        {100},            // single book
+        {10, 20}          // students > books
+    };
+
+    vector<int> students = {
+        2, // expected 113
+        2, // expected 60
+        2, // expected 10
+        1, // expected 100
+        3  // expected -1
+    };
+
+    vector<int> expected = {
+        113, 60, 10, 100, -1};
+
+    for (int i = 0; i < tests.size(); ++i)
+    {
+        cout << "Test " << i + 1 << "\n";
+        cout << "Books = ";
+        printVector(tests[i]);
+        cout << "\nStudents = " << students[i] << "\n";
+
+        int ans = allocateBooks(tests[i], students[i]);
+
+        cout << "Expected = " << expected[i] << "\n";
+        cout << "Actual   = " << ans << "\n\n";
+    }
+
+    return 0;
+}
