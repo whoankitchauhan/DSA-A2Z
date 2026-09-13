5cc2771b6986c4679c1bda32778affb4f9ec96b5
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-13 19:11:04 +0530

    Time: 0 ms (100%), Space: 28.7 MB (82.02%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        int frequency[10] = {};
-
-        for (int digit : digits) {
-            frequency[digit]++;
+        // Count the frequency of each digit available to us
+        int freq[10] = {0};
+        for (int d : digits) {
+            freq[d]++;
         }
 
         int count = 0;
 
-        for (int num = 100; num <= 999; num += 2) {
-            if (num % 2 != 0)
+        // Loop 'a' (hundreds place): Cannot be 0 for a 3-digit number
+        for (int a = 1; a <= 9; a++) {
+            if (freq[a] == 0)
                 continue;
+            freq[a]--; // Use one copy of digit 'a'
 
-            int a = num / 100;
-            int b = (num / 10) % 10;
-            int c = num % 10;
+            // Loop 'b' (tens place)
+            for (int b = 0; b <= 9; b++) {
+                if (freq[b] == 0)
+                    continue;
+                freq[b]--; // Use one copy of digit 'b'
 
-            int used[10] = {};
-            used[a]++;
-            used[b]++;
-            used[c]++;
-
-            bool possible = true;
-
-            for (int digit = 0; digit <= 9; digit++) {
-                if (used[digit] > frequency[digit]) {
-                    possible = false;
-                    break;
+                // Loop 'c' (ones place): Step by 2 to guarantee the number is
+                // even
+                for (int c = 0; c <= 8; c += 2) {
+                    if (freq[c] > 0) {
+                        count++; // A unique valid 3-digit even number is formed
+                    }
                 }
+
+                freq[b]++; // Backtrack: restore digit 'b'
             }
 
-            if (possible) {
-                count++;
-            }
+            freq[a]++; // Backtrack: restore digit 'a'
         }
 
         return count;
