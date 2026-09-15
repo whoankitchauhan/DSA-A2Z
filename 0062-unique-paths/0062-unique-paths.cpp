5b9bfa3686dde58aeb6fb90386fe77df7a4ec864
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-15 12:15:56 +0530

    Time: 0 ms (100%), Space: 9.4 MB (30.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    int solve(int row, int col, int m, int n, vector<vector<int>>& dp) {
-
-        if (row >= m || col >= n)
-            return 0;
-
-        if (row == m - 1 && col == n - 1)
-            return 1;
-
-        // Already calculated
-        if (dp[row][col] != -1)
-            return dp[row][col];
-
-        int down = solve(row + 1, col, m, n, dp);
-
-        int right = solve(row, col + 1, m, n, dp);
-
-        return dp[row][col] = down + right;
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(0, 0, m, n, dp);
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // First row
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // First column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill remaining cells
+        for (int i = 1; i < m; i++) {
+
+            for (int j = 1; j < n; j++) {
+
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
