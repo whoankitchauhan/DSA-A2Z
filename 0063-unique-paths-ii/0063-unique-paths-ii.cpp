fa9ab344b609f665ae70f88d8811d3f14ebd2aba
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-15 13:56:04 +0530

    Time: 0 ms (100%), Space: 11.6 MB (77.9%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,32 +5,21 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        if (grid[0][0] == 1)
-            return 0;
-
-        dp[0][0] = 1;
+        vector<int> dp(n, 0);
+        dp[0] = 1;
 
         for (int i = 0; i < m; i++) {
+
             for (int j = 0; j < n; j++) {
 
                 if (grid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
+                    dp[j] = 0;
+                } else if (j > 0) {
+                    dp[j] = dp[j] + dp[j - 1];
                 }
-
-                if (i == 0 && j == 0)
-                    continue;
-
-                if (i > 0)
-                    dp[i][j] += dp[i - 1][j];
-
-                if (j > 0)
-                    dp[i][j] += dp[i][j - 1];
             }
         }
 
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 };
