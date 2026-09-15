26166c6346b889b86c987ff2049f5274bba9244c
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-15 12:53:59 +0530

    Time: 0 ms (100%), Space: 8.3 MB (82.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> pathsPerRow(n, 1);
 
-        // First row
-        for (int j = 0; j < n; j++) {
-            dp[0][j] = 1;
-        }
+        for (int row = 1; row < m; row++) {
 
-        // First column
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
+            for (int col = 1; col < n; col++) {
 
-        // Fill remaining cells
-        for (int i = 1; i < m; i++) {
-
-            for (int j = 1; j < n; j++) {
-
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                pathsPerRow[col] = pathsPerRow[col] + pathsPerRow[col - 1];
             }
         }
 
-        return dp[m - 1][n - 1];
+        return pathsPerRow[n - 1];
     }
 };
