a32404f6e75e841475fa446aa4f670507d5c71ab
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-24 23:27:02 +0530

    Time: 0 ms (100%), Space: 31.1 MB (4.04%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int smallestIndex(vector<int>& nums) {
-        if (nums[0] == 0)
-            return 0;
-        for (int i = 1; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
             int sum = 0;
-            int temp = nums[i];
-            while (temp) {
-                sum += temp % 10;
-                temp = temp / 10;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
             }
-            if (sum == i) {
+
+            if (sum == i)
                 return i;
-            }
         }
+
         return -1;
     }
 };
