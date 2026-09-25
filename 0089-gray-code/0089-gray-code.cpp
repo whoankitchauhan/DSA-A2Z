ba68a74512b5039da4be04afb0de4bf6b494231b
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-09-26 00:01:24 +0530

    Time: 0 ms (100%), Space: 14.5 MB (83.27%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> ans;
-        int j = pow(2, n);
-        for (int i = 0; i < j; i++) {
-            int greyCode = i ^ (i >> 1);
-            ans.push_back(greyCode);
+
+        int total = 1 << n;
+
+        for (int i = 0; i < total; i++) {
+            ans.push_back(i ^ (i >> 1));
         }
+
         return ans;
     }
 };
